6fa1bd05ecc5dd641ab05de1b88826121d691897
Reza Chrismardianto <rezachrismardianto20@gmail.com>
2021-12-30 06:31:13 +0700

    update pagi

--- main.go
+++ main.go
@@ -1,27 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"pokemon-list/injector"
-	"strconv"
 )
 
-func FibonacciRecursion(n int) int {
-	if n <= 1 {
-		return n
-	}
-	return FibonacciRecursion(n-1) + FibonacciRecursion(n-2)
-}
-
 func main() {
-
-	for i := 0; i <= 9; i++ {
-		fmt.Print(strconv.Itoa(FibonacciRecursion(i)) + " ")
-	}
-	fmt.Println("")
-
 	s := injector.InitializedServer()
 	if err := s.ListenAndServe(); err != http.ErrServerClosed {
 		log.Fatal(err)
